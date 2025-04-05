8bb615075a0b2c7d20bd9263a6abfae4c4be6008
Pvngu <omargt151102@gmail.com>
2025-04-04 21:05:34 -0700

    Add new mail components and Vue.js store setup; remove unused bootstrap.js

---
+++ resources/js/main/router/index.js
@@ -0,0 +1,91 @@
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
+
+import AuthRoutes from "./auth";
+import DashboardRoutes from "./dashboard";
+import UserRoutes from "./users";
+import MessagingRoutes from "./messaging";
+import FormRoutes from "./forms";
+import SettingRoutes from "./settings";
+import superAdminRoutes from "../../superadmin/router/index";
+import subscriptionRoutes from "../../superadmin/router/admin/index";
+import { checkUserPermission } from "../../common/scripts/functions";
+
+const APP_TYPE = window.config.app_type;
+
+const checkCompanySetup = (isSuper = false) => {
+    const { appSetting } = store.state.auth;
+    if (isSuper) {
+        return (
+            appSetting.x_currency_id != null && appSetting.white_label_completed
+        );
+    }
+    return appSetting.x_currency_id != null;
+};
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "",
+            redirect: "/admin/login",
+        },
+        ...AuthRoutes,
+        ...superAdminRoutes,
+        ...subscriptionRoutes,
+        ...DashboardRoutes,
+        ...UserRoutes,
+        ...SettingRoutes,
+        ...MessagingRoutes,
+        ...FormRoutes,
+    ],
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+});
+
+router.beforeEach((to, from, next) => {
+    const { user } = store.state.auth;
+    const isLoggedIn = store.getters["auth/isLoggedIn"];
+    const isSuperAdmin = user?.is_superadmin;
+    store.commit("auth/updateAppChecking", false);
+
+    const redirectToLogin = () => {
+        store.dispatch("auth/logout");
+        next({ name: "admin.login" });
+    };
+
+    if (to.meta.requireAuth && !isLoggedIn) {
+        return redirectToLogin();
+    }
+
+    if (to.name.startsWith("superadmin")) {
+        if (to.meta.requireAuth && isLoggedIn && !isSuperAdmin) {
+            return redirectToLogin();
+        }
+        if (to.meta.requireUnauth && isLoggedIn) {
+            return next({ name: "superadmin.dashboard.index" });
+        }
+    } else if (to.name.startsWith("admin") && !to.name.startsWith("admin.login")) {
+        if (isSuperAdmin) {
+            return next({ name: "superadmin.dashboard.index" });
+        }
+        if (to.meta.requireUnauth && isLoggedIn) {
+            return next({ name: "admin.dashboard.index" });
+        }
+        if (to.name === `${APP_TYPE}.settings.modules.index`) {
+            return next();
+        }
+        const { permission } = to.meta;
+        if (!permission || checkUserPermission(permission, user)) {
+            return next();
+        }
+        return next({ name: "admin.dashboard.index" });
+    } else if (to.name.startsWith("front")) {
+        if (to.meta.requireAuth && !store.getters["front/isLoggedIn"]) {
+            store.dispatch("front/logout");
+            return next({ name: "front.homepage" });
+        }
+    }
+    next();
+});
+
+export default router;
---
+++ resources/js/superadmin/router/index.js
@@ -0,0 +1,19 @@
+import dashboard from "./dashboard";
+import settings from "./settings";
+import company from "./company";
+import paymentTranscation from "./paymentTranscation";
+import users from "./users";
+import subscriptionPlan from "./subscriptionPlan";
+import websiteSettings from "./websiteSettings";
+import emailQueries from "./emailQueries";
+
+export default [
+    ...dashboard,
+    ...settings,
+    ...company,
+    ...paymentTranscation,
+    ...users,
+    ...subscriptionPlan,
+    ...websiteSettings,
+    ...emailQueries,
+];
